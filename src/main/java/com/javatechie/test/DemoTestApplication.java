2f3ca61c9460b89ae3afe760c17c025f565bf463
parvez-ahmad <79909780+parvez-ahmad@users.noreply.github.com>
2021-05-20 09:27:56 +0530

    Changes done by developer 2

--- src/main/java/com/javatechie/test/DemoTestApplication.java
+++ src/main/java/com/javatechie/test/DemoTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoTestApplication {
 
+	public void test(){}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoTestApplication.class, args);
 		System.out.println("before commit");
